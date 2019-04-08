774ad2e08488be3dcdc370710fc3ec0865c513c4
nellyhsu123 <33136548+nellyhsu123@users.noreply.github.com>
2019-04-08 15:49:35 -0700

    updated app file

--- app.js
+++ app.js
@@ -14,4 +14,6 @@ var scores, roundScore, activePlayer, dice;
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
-dice = Math.floor(Math.Random() * 6) + 1;
+dice = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector('#current-0').textContent = dice;
