238b86159f8be572a97ee9b7ec10c143353bf75d
nellyhsu123 <33136548+nellyhsu123@users.noreply.github.com>
2019-04-09 20:22:22 -0700

    almost done with lesson 4

--- app.js
+++ app.js
@@ -11,17 +11,9 @@ GAME RULES:
 
 var scores, roundScore, activePlayer;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
+init();
 
 
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
-
-document.querySelector('.dice').style.display = 'none';
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
 	
@@ -40,34 +32,78 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 		roundScore += dice;
 		document.querySelector('#current-' + activePlayer).textContent = roundScore;
 	} else {
-		scores[activePlayer] += roundScore;
-		document.querySelector('#score-' + activePlayer) 
 		//next player
-		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-		roundScore = 0;
-
-
-		document.getElementById('current-0').textContent = '0';
-		document.getElementById('current-1').textContent = '0';
-
-		document.querySelector('.player-0-panel').classList.toggle('active');
-		document.querySelector('.player-1-panel').classList.toggle('active');
-
-		//document.querySelector('.player-0-panel').classList.remove('active');
-		//document.querySelector('.player-1-panel').classList.add('active');
-
-		document.querySelector('.dice').style.display = 'none';
+		nextPlayer();
 	}
 });
 
+document.querySelector('.btn-hold').addEventListener('click', function() {
+	// Add current score to global score
+	scores[activePlayer] += roundScore;
+
+
+	// Update the UI
+	document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+
+	//Check if player won the game
+	if(scores[activePlayer] >= 20) {
+		document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+		document.querySelector('.dice').style.display = 'none';
+		//rather than writing tons of code in javascript to manipulate css, 
+		//make a style class in css in which you can add and remove
+		document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+		document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+	} else {
+		nextPlayer();
+	}
+});
+
+//DRY = don't repeat yourself function
+function nextPlayer() {
+	activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+	roundScore = 0;
+
+
+	document.getElementById('current-0').textContent = '0';
+	document.getElementById('current-1').textContent = '0';
+
+	document.querySelector('.player-0-panel').classList.toggle('active');
+	document.querySelector('.player-1-panel').classList.toggle('active');
+
+	//document.querySelector('.player-0-panel').classList.remove('active');
+	//document.querySelector('.player-1-panel').classList.add('active');
+
+	document.querySelector('.dice').style.display = 'none';
+}
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init(){
+	scores = [0, 0];
+	roundScore = 0;
+	activePlayer = 0;
+
+	document.querySelector('.dice').style.display = 'none';
+
+	document.getElementById('score-0').textContent = '0';
+	document.getElementById('score-1').textContent = '0';
+	document.getElementById('current-0').textContent = '0';
+	document.getElementById('current-1').textContent = '0';
+
+	document.getElementById('name-0').textContent = 'Player 1';
+	document.getElementById('name-1').textContent = 'Player 2';
+
+	document.querySelector('.player-0-panel').classList.remove('winner');
+	document.querySelector('.player-1-panel').classList.remove('winner');
+	document.querySelector('.player-0-panel').classList.remove('active');
+	document.querySelector('.player-1-panel').classList.remove('active');
+	document.querySelector('.player-0-panel').classList.add('active');
+}
 
 //document.querySelector('#current-' + activePlayer).textContent = dice;
 
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice +'</>';
 
 //var x = document.querySelector('#score-0').textContent;
-
-
-
 
 
